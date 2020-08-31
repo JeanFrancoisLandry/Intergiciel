12caae966a41516f9458a2fae25c291d56a14aa0
Jean-Francois Landry <jflandry@prevost-architectural.com>
2020-08-31 17:08:24 -0400

    Net_DesignRouting

---
+++ Prevost/Net_DesignRouting.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Net_DesignRouting](
+	[Design Routing No] [int] IDENTITY(1,1) NOT NULL,
+	[Design Item No] [int] NOT NULL,
+	[Design Tool] [int] NOT NULL,
+	[Quote No] [int] NOT NULL,
+	[Version] [int] NULL,
+	[Order] [int] NULL,
+	[Operation No] [int] NOT NULL,
+	[Project No] [nvarchar](20) NOT NULL,
+	[Work Center No] [nvarchar](20) NOT NULL,
+	[SetUp Time] [nvarchar](max) NOT NULL,
+	[Run Time] [nvarchar](max) NOT NULL,
+	[Routing Link Code] [nvarchar](max) NOT NULL,
+	[Creation Timestamp] [datetime] NOT NULL,
+	[Creation User] [nvarchar](max) NOT NULL,
+	[Modification Timestamp] [datetime] NOT NULL,
+	[Modification User] [nvarchar](max) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Design Routing No] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
